aef908db6e0cadcedd2e472c1c17ab7656c458a1
JonnyCash <jonathanacp93@gmail.com>
2019-06-19 17:26:32 -0500

    add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-
 /**
  * main - Entry point
  *
@@ -8,6 +7,7 @@
 int main(void)
 {
 	char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 	 , sizeof(phrase));
 	return (1);
