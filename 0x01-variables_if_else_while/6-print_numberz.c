27b1d8ddcf81ae00ec54fcfcf10884fe45f45d54
JonnyCash <jonathanacp93@gmail.com>
2019-06-20 14:09:27 -0500

    add 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+int ch;
+
+for (ch = 0; ch <= 9; ch++)
+{
+putchar(ch % 10 + '0');
+}
+putchar('\n');
+return (0);
+
+}
