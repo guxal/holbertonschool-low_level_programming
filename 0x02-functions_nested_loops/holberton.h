5be1f85e34c808b3065426285b8f5b1cbe92aabf
JonnyCash <jonathanacp93@gmail.com>
2019-06-24 12:45:54 -0500

    add  1-alphabet.c and modify holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,10 @@
 #pragma once  
-////holberton.h file
-//#ifndef _HOLBERTON_H
-//#define _HOLBERTON_H
+//holberton.h file
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
 
 int _putchar(char c);
+
+void print_alphabet(void);
+
+#endif // _HOLBERTON_H
