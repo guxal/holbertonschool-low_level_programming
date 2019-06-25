2c432d3cab9d8003fc12b23fb018450cc1493ab5
JonnyCash <jonathanacp93@gmail.com>
2019-06-25 10:56:00 -0500

    add 5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_sign - print sign
+ * @n : data
+ *
+ * Return: Always num (Success)
+ */
+int print_sign(int n)
+{
+	char sign;
+	int num;
+
+	if (n > 0)
+	{
+		num = 1;
+		_putchar(43);
+	}
+	if (n < 0)
+	{
+		num = -1;
+		_putchar(45);
+	}
+	if (n == 0)
+	{
+		num = 0;
+		_putchar(48);
+	}
+
+	return (num);
+}
