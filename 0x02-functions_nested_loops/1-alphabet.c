0ff6a7256115b4f8982a29948980e7f04d84f3b1
JonnyCash <jonathanacp93@gmail.com>
2019-06-25 10:08:12 -0500

    change answer 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * print_alphabet - writes the alphabet
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 void print_alphabet(void)
 {
 	char c;
-	for(c = 'a' ; c <= 'z' ; c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
-	//return(0);
 }
