d507be68e74d2d0d06ad1fa3dbbaab524a351d1a
JonnyCash <jonathanacp93@gmail.com>
2019-06-25 17:16:54 -0500

    add data holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -7,10 +7,15 @@ int _islower(int c);
 
 int print_sign(int n);
 
+int _abs(int i);
+
 int _isalpha(int c);
+
+int print_last_digit(int);
 
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
 
+void jack_bauer(void);
 #endif
