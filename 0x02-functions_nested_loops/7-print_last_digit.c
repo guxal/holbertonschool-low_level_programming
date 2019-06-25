4d32058755b3dd874b8e4a353bad72888569ec60
JonnyCash <jonathanacp93@gmail.com>
2019-06-25 14:47:01 -0500

    change answer 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,21 @@
 #include "holberton.h"
 
 /**
- * _isalpha - writes the character c to stdout
+ * _abs - absoluted value
+ * @i: value
+ *
+ * Return: absoluted.
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = i * -1;
+	}
+	return (i);
+}
+/**
+ * print_last_digit - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
@@ -9,8 +23,9 @@
  */
 int print_last_digit(int c)
 {
-	int last = c%10;
+	int last = c % 10;
+
 	last = _abs(last);
-	_putchar(last+'0');
+	_putchar(last + '0');
 	return (last);
 }
