17e776935cec7ebae33b9b3ef18e33059f0f7776
JonnyCash <jonathanacp93@gmail.com>
2019-06-25 10:13:28 -0500

    change answer

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,21 @@
 #include "holberton.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * print_alphabet_x10 - print alphabet x10
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 void print_alphabet_x10(void)
 {
-  char c;
+	char c;
 	int i;
 
-	for (i = 0; i <= 10; i++){
-  	for (c = 'a' ; c <= 'z' ; c++)
-  	{
-    	_putchar(c);
-  	}
- 		_putchar('\n');
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
-  //return(0);
 }
