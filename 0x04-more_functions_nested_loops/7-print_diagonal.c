2e20008608de881be8d0a62d949c56d60abb9a6a
JonnyCash <jonathanacp93@gmail.com>
2019-06-27 13:42:37 -0500

    add 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_diagonal - print diagonal
+ * @n: value
+ *
+ * Return: none
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+	
+	for (i = 0; i < n; i++)
+	{
+		//for (j = 0; j < n; j++)
+		//	_putchar(32);
+		for (j = 0; j < i; j++)
+			_putchar(32);
+		_putchar(92);
+		_putchar('\n');
+	}
+	if(n <= 0)
+		_putchar('\n');
+}
