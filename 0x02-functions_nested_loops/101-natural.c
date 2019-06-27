71bab969c52631626fd5d08feb323518935dc597
JonnyCash <jonathanacp93@gmail.com>
2019-06-26 22:20:39 -0500

    change answer

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,9 +12,7 @@ void print_natural(void)
 
 	for (i = 0; i <= 1024; i++)
 	{
-		if (i % 3 == 0)
-			sum = sum + i;
-		if (i % 5 == 0)
+		if ((i % 3 || i % 5) == 0)
 			sum = sum + i;
 	}
 	printf("%d\n", sum);
