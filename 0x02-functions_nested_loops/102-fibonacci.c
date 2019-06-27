b11e3c22db15c1766c59f6813e070c5ceb4c804e
JonnyCash <jonathanacp93@gmail.com>
2019-06-27 09:54:52 -0500

    add 102-fibonnaci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * fibonacci - print fibonacci 50 first numbers
+ *
+ * Return: none
+ */
+void fibonacci(void)
+{
+	int i;
+	long long mult = 0;
+	int last = 1;
+
+	for (i = 0; i <= 50; i++)
+	{
+		int current;
+
+		current = mult;
+		mult = mult + last;
+		last = current;
+		printf("%1lld", mult);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+}
+/**
+  * main - entry point
+  *
+  * Return: success 0
+  */
+int main(void)
+{
+	fibonacci();
+	return (0);
+}
+
