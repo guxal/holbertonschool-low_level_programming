b497df9c35723836529a9edb0fa37a6f8ee69f1a
JonnyCash <jonathanacp93@gmail.com>
2019-06-27 10:59:29 -0500

    add 1-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+ * _isdigit - is a digit
+ *
+ * Return:0
+ */
+int _isdigit(int c)
+{
+	int v = 0;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if(c >= 48 && c <=  58)
+		v = 1;
+	}
+	return (v);
+}
