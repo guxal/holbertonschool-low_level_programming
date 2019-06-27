e2ebb9431e3eca4ea2b588080b88088951e17a2f
JonnyCash <jonathanacp93@gmail.com>
2019-06-27 12:41:31 -0500

    add data

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -3,6 +3,17 @@
 
 #include <stdio.h>
 
+int _isdigit(int c);
+
 int _isupper(int c);
 
+int mul(int a, int b);
+
+int _putchar(char c);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
 #endif /* HOLBERTON_H */
