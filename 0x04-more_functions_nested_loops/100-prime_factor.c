8be78c49345bc7c2c3140bc42c995479d85c3fb0
JonnyCash <jonathanacp93@gmail.com>
2019-06-29 20:42:59 -0500

    hot fix

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
-
-void print_prime_factor(void);
 /**
  * main - Entry Point
  *
- * Return: success 0
+ * Return: none
  */
 int main(void)
 {
-	print_prime_factor();
-	return (0);
-}
-/**
- * print_prime_factor - print factor prime
- *
- * Return: none
- */
-void print_prime_factor(void)
-{
-	unsigned long long num = 612852475143;
-	unsigned int i;
+	long num = 612852475143;
+	long i;
 
 	for (i = 2; i < num; i++)
 	{
@@ -29,6 +17,7 @@ void print_prime_factor(void)
 		}
 	}
 
-	printf("%d\n", i);
+	printf("%li\n", i);
 
+	return (0);
 }
