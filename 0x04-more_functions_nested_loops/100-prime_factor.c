73af4e18755428406c9c81086a0420056207d9c1
JonnyCash <jonathanacp93@gmail.com>
2019-06-29 20:29:29 -0500

    add 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "holberton.h"
+
+void print_prime_factor(void);
+/**
+ * main - Entry Point
+ *
+ * Return: success 0
+ */
+int main(void)
+{
+	print_prime_factor();
+	return (0);
+}
+/**
+ * print_prime_factor - print factor prime
+ *
+ * Return: none
+ */
+void print_prime_factor(void)
+{
+	unsigned long long num = 612852475143;
+	int i;
+
+	for (i = 2; i <= num; i++)
+	{
+		if (num % i == 0)
+		{
+
+		/*
+		 *	printf("divisiones %d\n", i);
+		 *	printf("numero resultado : %lld\n", num);
+		 */
+
+			num = num / i;
+			i--;
+		}
+	}
+
+	printf("final : %d\n", i);
+
+}
