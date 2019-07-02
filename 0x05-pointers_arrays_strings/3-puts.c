d9022efc57ba3c39a772bf6020786b93c14ca8d5
JonnyCash <jonathanacp93@gmail.com>
2019-07-02 09:24:19 -0500

    hot fix

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,26 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * _strlen - strlen data
+ * @s: data
+ * Return: n
  */
 int _strlen(char *s)
 {
 	int n = 0;
 
-	while (s[n]) ++n;
+	while (s[n])
+		++n;
 	return (n);
 }
-
+/**
+ * _puts - puts stdout
+ * @str: string print
+ * Return: none
+ */
 void _puts(char *str)
 {
 	int i;
+
 	for (i = 0; i <= _strlen(str); i++)
 	{
 		_putchar(str[i]);
