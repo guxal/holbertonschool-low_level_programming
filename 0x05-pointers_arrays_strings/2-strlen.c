0ef4ff9e39e4fb4add31ed70f726952af26d46c1
JonnyCash <jonathanacp93@gmail.com>
2019-07-02 09:21:36 -0500

    hot fix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-
+/**
+ * _strlen - strlen data
+ * @s: data
+ * Return: n
+ */
 int _strlen(char *s)
 {
 	int n = 0;
 
-	while (s[n]) ++n;
+	while (s[n])
+		++n;
 
 	return (n);
 }
