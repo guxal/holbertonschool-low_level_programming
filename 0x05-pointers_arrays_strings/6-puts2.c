6458e80b970467aba3bbeb3d7c2cca71f8c5ace8
JonnyCash <jonathanacp93@gmail.com>
2019-07-02 14:36:09 -0500

    add 6-puts.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * _strlen - strlen data
+ * @s: data
+ * Return: n
+ */
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n])
+		++n;
+
+	return (n);
+}
+/**
+ * puts2 - puts
+ * @str: string
+ * Return: none
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(str); i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar(10);
+}
