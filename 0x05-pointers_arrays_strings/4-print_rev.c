ab3d843d3a016e88ffb3ca7e1f138070d0fb6e62
JonnyCash <jonathanacp93@gmail.com>
2019-07-02 09:43:33 -0500

    hot fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,27 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * _strlen - strlen count
+ * @s: data
+ * Return: data
  */
 int _strlen(char *s)
 {
 	int n = 0;
 
-	while (s[n]) ++n;
+	while (s[n])
+		++n;
+
 	return (n);
 }
-
+/**
+ * print_rev - print reverse
+ * @s: data
+ * Return: data
+ */
 void print_rev(char *s)
 {
 	int i;
+
 	for (i = _strlen(s); i >= 0; i--)
 	{
 		_putchar(s[i]);
