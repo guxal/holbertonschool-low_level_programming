1fca2ed51430c310b7b254f14ff1e1fb8e0f5e42
JonnyCash <jonathanacp93@gmail.com>
2019-07-03 18:10:39 -0500

    add 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,45 @@
+/**
+ * _strlen - strlen data
+ * @s: data
+ * Return: n
+ */
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n])
+		++n;
+
+	return (n);
+}
+/**
+ * _atoi - convert int to string
+ * @s: string
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int str = _strlen(s);
+	int i;
+	int sign = 1;
+	int sum = 0;
+
+	for (i = 0; i <= str; i++)
+	{
+		if (s[i] == '-')
+			sign = sign * -1;
+		if (s[i] == '+')
+			sign = sign * +1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			sum = (s[i] - 48)  + (sum * 10);
+		}
+		if (sum > 0 && s[i] == ' ')
+			break;
+	}
+
+	if (sign < 0)
+		sum = sum * -1;
+
+	return (sum);
+}
