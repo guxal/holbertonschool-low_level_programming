3610da7646aa3cf0fdcdaf96bdf9babb59f11f14
JonnyCash <jonathanacp93@gmail.com>
2019-07-08 13:18:19 -0500

    hotfi

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,13 +7,9 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
 	char *p = s;
 
-	for (i = 0; i < n; i++)
-	{
-		*(p + i) = b;
-	}
-
+	while (n--)
+		*p++ = b;
 	return (s);
 }
