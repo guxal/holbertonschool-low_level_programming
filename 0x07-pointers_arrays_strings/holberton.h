fb93ffb7350f162bdf3f20d1b8862bf374412443
JonnyCash <jonathanacp93@gmail.com>
2019-07-08 12:39:38 -0500

    holberton.h

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+char *_strchr(char *s, char c);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* HOLBERTON_H */
