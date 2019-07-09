c4a29a7f08b75df3040dd8db4ef6c31e1d223d8d
JonnyCash <jonathanacp93@gmail.com>
2019-07-08 23:01:35 -0500

    holberton

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,6 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+void print_chessboard(char (*a)[8]);
+
+char *_strstr(char *haystack, char *needle);
+
+char *_strpbrk(char *s, char *accept);
+
+unsigned int _strspn(char *s, char *accept);
+
 int _putchar(char c);
 
 char *_strchr(char *s, char c);
