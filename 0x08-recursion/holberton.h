4bc3514c0e98b68e644a2adfce990f3ed02718f2
JonnyCash <jonathanacp93@gmail.com>
2019-07-10 12:03:08 -0500

    holberton

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,10 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+int _strlen_recursion(char *s);
+
 void _print_rev_recursion(char *s);
 
 int _putchar(char c);
 
 void _puts_recursion(char *s);
+
+int factorial(int n);
 
 #endif /* HOLBERTON_H */
