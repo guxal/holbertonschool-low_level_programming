35decc594f96351151316b88eae45d50808b2179
JonnyCash <jonathanacp93@gmail.com>
2019-07-16 14:56:40 -0500

    add 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+/**
+ * *_strdup - duplicate string
+ * @str: string
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+	char *new;
+	int i = 0;
+
+	new = malloc(sizeof(str));
+
+	if (!new || *str == 0)
+		return (0);
+	while (str[i])
+	{
+		new[i] = str[i];
+		i++;
+	}
+
+	return (new);
+}
