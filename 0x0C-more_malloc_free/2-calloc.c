b131fbe57a28984f44fe5d44be030d77b1d7c417
JonnyCash <jonathanacp93@gmail.com>
2019-07-18 17:11:41 -0500

    hotfix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (memory == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
-		*(memory + (size * i)) = 0;
+	for (i = 0; i < nmemb * size; i++)
+		*(memory + i) = 0;
 	return ((void *)memory);
 }
