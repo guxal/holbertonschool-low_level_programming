2a9e52314f8c5db5d0b40e912315ca5559590bbe
JonnyCash <jonathanacp93@gmail.com>
2019-07-18 13:35:38 -0500

    add 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+/**
+ * _strlen - string length
+ * @str: string
+ * Return: length string
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		++i;
+	return (i);
+}
+
+/**
+ * *string_nconcat - string concatenation
+ * @s1: char 1
+ * @s2: char 2
+ * @n: number
+ * Return: new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *newstring;
+	int strlen_s1;
+	unsigned int strlen_s2;
+	int _malloc, i;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	strlen_s1 = _strlen(s1);
+	strlen_s2 = _strlen(s2);
+
+	if (strlen_s2 <= n)
+		_malloc = strlen_s1 + strlen_s2;
+	else
+		_malloc = strlen_s1 + n;
+
+	newstring = malloc(sizeof(char) * _malloc);
+
+	for (i = 0; i < _malloc; i++)
+	{
+		if (i < strlen_s1)
+			newstring[i] = s1[i];
+		else
+			newstring[i] = s2[i - strlen_s1];
+	}
+	newstring[i] = '\0';
+	return (newstring);
+}
