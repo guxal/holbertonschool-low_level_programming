3929160653659f9ca067556922381014d65b537b
JonnyCash <jonathanacp93@gmail.com>
2019-07-18 14:37:07 -0500

    add 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * *_calloc - call
+ * @nmemb: n
+ * @size: size
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *memory;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	memory = malloc(nmemb * size);
+	if (memory == NULL)
+		return (NULL);
+	return (memory);
+}
