bdb81560ea34074fccb331a8b924d57debcfb2b6
JonnyCash <jonathanacp93@gmail.com>
2019-07-22 17:43:01 -0500

    hotfix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,7 +9,8 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *cnew_dog;
+	dog_t *cnew_dog;
+	char *copyn, *copyo;
 
 	cnew_dog = malloc(sizeof(struct dog));
 	if (cnew_dog == NULL)
@@ -17,5 +18,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	cnew_dog->name = name;
 	cnew_dog->age = age;
 	cnew_dog->owner = owner;
+
+	copyn = cnew_dog->name;
+	copyo = cnew_dog->owner;
+
 	return (cnew_dog);
 }
