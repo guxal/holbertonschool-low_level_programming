d9f52548969605ca583c8c8326843392dfa644c3
JonnyCash <jonathanacp93@gmail.com>
2019-07-22 11:41:05 -0500

    add 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+ * *new_dog - new dog
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner dog
+ * Return: new dog struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog newdog;
+	struct dog *cnew_dog;
+
+	newdog.name = name;
+	newdog.age = age;
+	newdog.owner = owner;
+	cnew_dog = &newdog;
+	return (cnew_dog);
+}
