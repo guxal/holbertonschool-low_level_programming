eaa8bebc50e79145d439fa98ef0a91ddd9427151
JonnyCash <jonathanacp93@gmail.com>
2019-07-22 12:05:57 -0500

    add betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,28 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+/**
+ * struct dog - struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: struct
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 #endif /* HOLBERTON_H */
