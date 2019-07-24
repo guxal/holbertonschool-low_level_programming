72d780fa19f517b0ae6aca59f70b167dc7ddfc49
JonnyCash <jonathanacp93@gmail.com>
2019-07-24 01:20:20 -0500

    add 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+/**
+ * int_index - integer index
+ * @array: array data
+ * @size: size array
+ * @cmp: compare function
+ * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
