112573bf3a3ce74453de2024d413a745af1d9c1e
JonnyCash <jonathanacp93@gmail.com>
2019-08-01 14:02:31 -0500

    hotfix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,11 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h->next != '\0')
+	while (h)
 	{
 		printf("[%d] %s\n", (h->str != '\0') ?
 				h->len : 0, (h->str != '\0') ? h->str : "(nil)");
 		h = h->next;
-		if (h->next == NULL)
-		{
-			printf("[%d] %s\n", (h->str != '\0') ?
-					h->len : 0, (h->str != '\0') ? h->str : "(nil)");
-			count++;
-		}
 		count++;
 	}
 
