4cd9cbac9da25e20a2536fb782a04b4210b12dd8
JonnyCash <jonathanacp93@gmail.com>
2019-08-01 13:55:48 -0500

    add null in len

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,11 +12,11 @@ size_t print_list(const list_t *h)
 
 	while (h->next != '\0')
 	{
-		printf("[%d] %s\n", h->len, (h->str != '\0') ? h->str : "(nil)");
+		printf("[%d] %s\n", (h->str != '\0') ? h->len : 0, (h->str != '\0') ? h->str : "(nil)");
 		h = h->next;
 		if (h->next == NULL)
 		{
-			printf("[%d] %s\n", h->len, (h->str != '\0') ? h->str : "(nil)");
+			printf("[%d] %s\n", (h->str != '\0') ? h->len : 0, (h->str != '\0') ? h->str : "(nil)");
 			count++;
 		}
 		count++;
