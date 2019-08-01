f3e9044f5b8d391e444ac19ede5b20ebdda9d415
JonnyCash <jonathanacp93@gmail.com>
2019-08-01 13:51:28 -0500

    init project and add files and firts task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * print_list - print list
+ * @h: list
+ * Return: integer
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h->next != '\0')
+	{
+		printf("[%d] %s\n", h->len, (h->str != '\0') ? h->str : "(nil)");
+		h = h->next;
+		if (h->next == NULL)
+		{
+			printf("[%d] %s\n", h->len, (h->str != '\0') ? h->str : "(nil)");
+			count++;
+		}
+		count++;
+	}
+
+	return (count);
+}
