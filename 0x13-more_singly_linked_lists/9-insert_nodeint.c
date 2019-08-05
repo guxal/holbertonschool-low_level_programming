bccb69ba38dfc0746872ebe2eed44dd033045c45
JonnyCash <jonathanacp93@gmail.com>
2019-08-05 18:11:09 -0500

    add9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include "stdlib.h"
+/**
+ * *insert_nodeint_at_index - insert node int
+ * @head: head node list
+ * @idx: index
+ * @n: integer in n new
+ * Return: integer
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int count = 0;
+	listint_t *new;
+	listint_t *tmp;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	tmp = *head;
+	while (tmp)
+	{
+		if ((count + 1) == idx)
+		{
+			new->n = n;
+			new->next = tmp->next;
+			break;
+		}
+		tmp = tmp->next;
+		count++;
+	}
+	tmp->next = new;
+
+	return (tmp);
+}
