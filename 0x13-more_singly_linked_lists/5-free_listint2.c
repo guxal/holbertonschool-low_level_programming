aec3c51f4d20a1f767e71751b00585b872e8270b
JonnyCash <jonathanacp93@gmail.com>
2019-08-06 10:10:59 -0500

    add 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head: head node list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
