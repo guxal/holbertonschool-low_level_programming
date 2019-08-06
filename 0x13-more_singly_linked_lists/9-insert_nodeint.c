66edaeff8ec42b460a479a330a796491613e4f7f
JonnyCash <jonathanacp93@gmail.com>
2019-08-06 14:34:57 -0500

    update delete lines

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,20 +23,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	else
-		tmp = *head;
-	while (tmp != NULL)
+	tmp = *head;
+	while (tmp)
 	{
 		if ((count + 1) == idx)
 		{
 			new->next = tmp->next;
 			tmp->next = new;
-			return (tmp);
-		}
-		else if (idx == 0)
-		{
-			new->next = *head;
-			*head = new;
 			return (new);
 		}
 		tmp = tmp->next;
