63bb0b66e860cf145ed715a29fc8accadd2bc46b
JonnyCash <jonathanacp93@gmail.com>
2019-08-06 12:00:25 -0500

    hotfix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,18 +25,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new->n = n;
 			new->next = tmp->next;
-			break;
+			tmp->next = new;
+			return (tmp);
 		}
 		else if (idx == 0)
 		{
 			new->n = n;
-			new->next = tmp->next;
-			break;
+			new->next = tmp;
+			*head = new;
+			return (*head);
 		}
 		tmp = tmp->next;
 		count++;
 	}
-	tmp->next = new;
-
-	return (tmp);
+	return (NULL);
 }
