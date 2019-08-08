323fb11b4e27a23a5f23efa6c6956fbb923427b7
JonnyCash <jonathanacp93@gmail.com>
2019-08-08 17:18:22 -0500

    add 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+/**
+ * _strlen - string length
+ * @str: string count
+ * Return: strlen
+ */
+int _strlen(const char *str)
+{
+	int count = 0;
+
+	while (str[count])
+		++count;
+	return (count);
+}
+/**
+ * binary_to_uint - binary convert
+ * @b: binary
+ * Return: integer convert
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	int count = 0;
+	int base = 1;
+	int strlen = _strlen(b) - 1;
+
+	while (b[count])
+	{
+		if (b[count] != 48 && b[count] != 49)
+			return (0);
+		decimal += (b[strlen - count] - 48) * base;
+		base = base * 2;
+		count++;
+	}
+	return (decimal);
+}
