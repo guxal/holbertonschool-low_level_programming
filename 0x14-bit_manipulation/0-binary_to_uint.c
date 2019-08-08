27dcd297b1c2ef6c06a6c6c09cbbbf449bdc20cd
JonnyCash <jonathanacp93@gmail.com>
2019-08-08 17:22:43 -0500

    add library stdlib

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * _strlen - string length
  * @str: string count
@@ -23,7 +24,7 @@ unsigned int binary_to_uint(const char *b)
 	int base = 1;
 	int strlen = _strlen(b) - 1;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
 	while (b[count])
 	{
