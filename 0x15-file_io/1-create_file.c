397c350ec67199ff5c5e1eef7003e8462f9d371a
JonnyCash <jonathanacp93@gmail.com>
2019-08-13 17:09:26 -0500

    hotfix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int _sstrlen(char *str)
 /**
  * create_file - new file
  * @filename: name
- * @text_content
+ * @text_content: text_content
  * Return: integer
  */
 int create_file(const char *filename, char *text_content)
@@ -25,14 +25,19 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int _strlen;
 
-	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0600);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
 		_strlen = _sstrlen(text_content);
-		write(fd, text_content, _strlen);
+		if (write(fd, text_content, _strlen) != _strlen)
+		{
+			return (-1);
+		}
 	}
 
 	close(fd);
