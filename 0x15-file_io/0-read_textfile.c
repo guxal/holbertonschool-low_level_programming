8cebefd3bdddd0c9f67e49172c51a129e51b2231
JonnyCash <jonathanacp93@gmail.com>
2019-08-13 14:49:16 -0500

    hotfix delete less -1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc((sizeof(char) * letters) - 1);
+	buf = malloc((sizeof(char) * letters));
 	if (buf == NULL)
 		return (0);
 	_strlen = read(fd, buf, letters);
