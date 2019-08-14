99d88bdae5ebf21c0b7084de58d1834a31cdd5a4
JonnyCash <jonathanacp93@gmail.com>
2019-08-14 01:06:17 -0500

    hotfix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,20 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include "holberton.h"
+/**
+ * RETURN_N - return posix
+ * @value: value
+ * @string: string
+ * @str: param
+ * Return: exit
+ */
+void RETURN_N(int value, char *string, char *str)
+{
+	dprintf(STDERR_FILENO, string, str);
+	exit(value);
+}
 /**
  * main - cp files
  * @argc: argument count
@@ -16,25 +29,25 @@ int main(int argc, char *argv[])
 	char buf[1024];
 
 	if (argc != 3)
-		return (__RETURN__(97, "Usage: cp file_from file_to"));
+		RETURN_N(97, "Usage: cp file_from file_to\n", "");
 	src = open(argv[1], O_RDONLY);
 	if (src == -1)
-		return (__RETURN__(98, "Error: Can't read from file NAME_OF_THE_FILE"));
+		RETURN_N(98, "Error: Can't read from file %s\n", argv[1]);
 	_strlen = read(src, buf, 1024);
 	dest = open(argv[2], O_WRONLY | O_APPEND | O_CREAT | O_TRUNC, 0664);
 	if (dest == -1)
-		return (__RETURN__(99, "Error: Can't write to NAME_OF_THE_FILE"));
+		RETURN_N(99, "Error: Can't write to %s\n", argv[2]);
 	while (_strlen != 0)
 	{
 		if (write(dest, buf, _strlen) != _strlen)
-			return (__RETURN__(99, "Error: Can't write to"));
+			RETURN_N(99, "Error: Can't write to %s\n", argv[2]);
 		_strlen = read(src, buf, 1024);
 		if (_strlen == -1)
-			return (__RETURN__(98, "Error: Can't read from file"));
+			RETURN_N(98, "Error: Can't read from file %s\n", argv[1]);
 	}
 	if (close(src) == -1)
-		return (__RETURN__(100, "Error: Can't close fd"));
+		RETURN_N(100, "Error: Can't close fd %s\n", "-1");
 	if (close(dest) == -1)
-		return (__RETURN__(100, "Error: Can't close fd"));
+		RETURN_N(100, "Error: Can't close fd %s\n", "-1");
 	return (0);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,11 +1,5 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#define __RETURN__(value, str) \
-	(__extension__ \
-	 ({ \
-	  printf("%s\n", str); \
-	  value; \
-	  }))
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
