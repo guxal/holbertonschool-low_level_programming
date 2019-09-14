27908f3dd8960b9035e1184d074d2ce8ef94ce29
JonnyCash <jonathanacp93@gmail.com>
2019-09-14 13:46:03 -0500

    add 2-add_dnodeint.c and new functions lists.h

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add node beginning
+ * @head: header node
+ * @n: integer n
+ * Return: new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
