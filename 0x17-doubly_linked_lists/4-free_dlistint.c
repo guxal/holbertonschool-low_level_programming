e5eceac746de5ad2d3afbef029704f45716d9554
JonnyCash <jonathanacp93@gmail.com>
2019-09-14 21:20:32 -0500

    hotfix betty

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,16 @@
 #include "lists.h"
+/**
+ * free_dlistint - free_dlistint
+ * @head: header
+ * Return: header
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
 
 	tmp = head;
 
-	while(head != NULL)
+	while (head != NULL)
 	{
 		tmp = head;
 		head = head->next;
