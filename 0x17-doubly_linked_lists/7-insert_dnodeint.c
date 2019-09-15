346b5bf74815a91d7cd0f131a7b4a7460fac3481
JonnyCash <jonathanacp93@gmail.com>
2019-09-15 01:41:53 -0500

    new conditio

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,22 @@
 #include "lists.h"
 /**
+ * dlistint_len - dlistint length
+ * @h: header
+ * Return: legth node
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
+/**
  * *insert_dnodeint_at_index - insert node in index
  * @h: header
  * @idx: index
@@ -9,6 +26,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int count = 0;
+	size_t len;
 	dlistint_t *new;
 	dlistint_t *tmp;
 
@@ -18,8 +36,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->prev = NULL;
 
+	len = dlistint_len(*h);
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
+
+	if (len == idx)
+		return (add_dnodeint_end(h, n));
 
 	tmp = *h;
 
