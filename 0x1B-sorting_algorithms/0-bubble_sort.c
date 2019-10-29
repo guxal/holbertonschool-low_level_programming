e647ae456584ab7ea9f9008010165ea0a8fc6769
JonnyCash <jonathanacp93@gmail.com>
2019-10-29 17:21:34 -0500

    initial commit project 0x1B-sorting algorithms

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * swap - swapped integers
+ * @a: value 1
+ * @b: value 2
+ **/
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+/**
+ * bubble_sort - sorts an array of integers in ascending order.
+ * @array: unsorted array that need to sort
+ * @size: the size of array
+ **/
+void bubble_sort(int *array, size_t size)
+{
+	bool swapped = false;
+	size_t i;
+
+	for (i = 1; i <= size - 1; i++)
+	{
+		if (array[i - 1] > array[i])
+		{
+			swap(&array[i - 1], &array[i]);
+			swapped = true;
+		}
+	}
+	if (swapped)
+	{
+		print_array(array, size);
+		bubble_sort(array, size);
+	}
+}
